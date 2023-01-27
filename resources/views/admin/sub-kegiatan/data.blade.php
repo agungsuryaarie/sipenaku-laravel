c5376c1bd1fac5c6d9c1de875a9dd89b6142aff3
abiihutabarat29 <abiihutabarat97@gmail.com>
2023-01-27 18:24:31 +0700

    update UI

--- resources/views/admin/sub-kegiatan/data.blade.php
+++ resources/views/admin/sub-kegiatan/data.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 @section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Sub Kegiatan</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ '/' }}">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Sub Kegiatan</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
     <section class="content">
         <div class="container-fluid">
             <div class="row">
